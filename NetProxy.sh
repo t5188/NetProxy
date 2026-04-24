b19c9b821a5a99fd1fc5236588a3225e52d3ec93
coomsn <coomsn@qq.com>
2026-04-24 14:57:08 +0800

    update

--- NetProxy.sh
+++ NetProxy.sh
@@ -1,11 +1,9 @@
 #!/system/bin/sh
 scripts_dir="/data/adb/NetProxy/scripts"
-. "${scripts_dir}/settings.ini"
 (
   until [ "$(getprop sys.boot_completed)" = "1" ]; do
     sleep 3
   done
   "${scripts_dir}/start.sh"
 ) &
-inotifyd "${scripts_dir}/NetProxy.inotify" "${module_dir}" >/dev/null 2>&1 &
 exit 0
