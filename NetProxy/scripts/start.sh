f83faf303b9a98fd58b821c129d30cca3fd3d536
coomsn <coomsn@qq.com>
2026-04-24 14:50:25 +0800

    update

--- NetProxy/scripts/start.sh
+++ NetProxy/scripts/start.sh
@@ -1,7 +1,6 @@
 #!/system/bin/sh
 scripts_dir="$(cd "$(dirname "$0")" && pwd)"
 . "${scripts_dir}/settings.ini"
-
 proxy_service() {
   if [[ ! -f "${module_dir}/disable" ]]; then
     ${scripts_dir}/NetProxy.service enable >/dev/null 2>&1
@@ -9,17 +8,4 @@ proxy_service() {
     toast "Module Disabled"
   fi
 }
-
-start_inotifyd() {
-  for PID in $(busybox pidof inotifyd 2>/dev/null); do
-    if grep -q "${scripts_dir}/NetProxy.inotify" "/proc/$PID/cmdline"; then
-      return
-    fi
-  done
-  inotifyd "${scripts_dir}/NetProxy.inotify" "${module_dir}" >/dev/null 2>&1 &
-}
-
 proxy_service
-start_inotifyd
-
-# Last edited: 2026.4.20
